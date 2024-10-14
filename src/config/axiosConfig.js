b0f6b65195000fb225d4b449d8918a4d41691d24
Miguel Ángel Arias <84602627+MiguelArias0316@users.noreply.github.com>
2024-10-13 22:13:22 -0500

    url back

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosConfig = axios.create({
-    /* baseURL: process.env.BASE_URL_API || 'https://peliculas-back.onrender.com/api/v1' */
-    baseURL: process.env.BASE_URL_API || '//localhost:4000/api/v1'
+    baseURL: process.env.BASE_URL_API || 'https://peliculas-back.onrender.com/api/v1'
+/*     baseURL: process.env.BASE_URL_API || '//localhost:4000/api/v1' */
 })
