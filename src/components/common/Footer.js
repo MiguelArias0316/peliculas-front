74b04155008def7afae92c5cb9367443819e92a5
Miguel Ángel Arias <84602627+MiguelArias0316@users.noreply.github.com>
2024-10-13 22:19:49 -0500

    footer fix

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -2,11 +2,11 @@ export default function Footer() {
   return (
     <footer className="py-3 my-4">
             <ul className="nav justify-content-center border-bottom pb-3 mb-3">
-                <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Home</a></li>
-                <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Generos</a></li>
-                <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Directores</a></li>
-                <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Productoras</a></li>
-                <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Gestion Media</a></li>
+                <li className="nav-item"><a href="a" className="nav-link px-2 text-body-secondary">Home</a></li>
+                <li className="nav-item"><a href="a" className="nav-link px-2 text-body-secondary">Generos</a></li>
+                <li className="nav-item"><a href="a" className="nav-link px-2 text-body-secondary">Directores</a></li>
+                <li className="nav-item"><a href="a" className="nav-link px-2 text-body-secondary">Productoras</a></li>
+                <li className="nav-item"><a href="a" className="nav-link px-2 text-body-secondary">Gestion Media</a></li>
             </ul>
             <p className="text-center text-body-secondary">© 2024 Miguel Ángel Arias</p>
         </footer>
